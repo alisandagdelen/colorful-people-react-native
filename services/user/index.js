6cb0962ea950f892327a6c0121dcce879a36c013
Ozan Kasikci <kasikciozan@yandex.com>
2017-09-16 23:10:12 +0300

    remove automatic signup

--- services/user/index.js
+++ services/user/index.js
@@ -8,8 +8,6 @@ export const signupUser = async (username, password) => {
   return await firebase.auth().createUserWithEmailAndPassword(username, password);
 };
 
-signupUser('test1@gmail.com', 'test22').then(r => console.log(r)).catch(e => console.log(e))
-
 export default {
   loginUser,
   signupUser,
