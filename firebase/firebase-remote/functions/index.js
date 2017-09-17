b72f717acbf8488c7480708f17e4927d7f37d185
Ozan Kasikci <kasikciozan@yandex.com>
2017-09-17 18:42:29 +0300

    add firebase remote folder

---
+++ firebase/firebase-remote/functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const randomstring = require('randomstring');
+admin.initializeApp(functions.config().firebase);
+
+const generateColorId = (colorCount) => {
+  let string = '';
+
+  for(let i = 0; i < colorCount; i++) {
+    const color = randomstring.generate({length: 1, charset: '0123456'});
+    const character = randomstring.generate({length: 1, charset: 'alphanumeric', capitalization: 'lowercase'})
+    string += `${color}${character}`
+  }
+
+  return string;
+}
+
+exports.afterUserCreate = functions.auth.user().onCreate(event => {
+  const data = {
+    email: event.data.email,
+    colorId: generateColorId(4)
+  }
+  admin.database().ref(`/users/${event.data.uid}`).set(data)
+});
