2d46263c42b69250c2449a5fcfae2f3d4395dbf4
Ozan Kasikci <kasikciozan@yandex.com>
2017-12-20 23:25:47 +0300

    start implementing flowjs

--- services/message-service.js
+++ services/message-service.js
@@ -1,6 +1,16 @@
+// @flow
+
 import firebase from '../firebase/index';
 
-export const createMessage = async (chatUid, sender, to, content) => {
+type createMessageReturn = Promise<{ key: string, sender: string, to: string, chatUid: string }>;
+
+export const createMessage = async (
+  chatUid: string,
+  sender: string,
+  to: string,
+  content: string
+): createMessageReturn => {
+
   const messagesRef = firebase.database().ref(`messages`);
   const newMessageRef = messagesRef.push();
   const key = newMessageRef.key;
--- services/user-service.js
+++ services/user-service.js
@@ -1,16 +1,18 @@
+// @flow
+
 import firebase from '../firebase/index';
 
-export const getUserInfo = async (uid) => {
+export const getUserInfo = async (uid: string) => {
   return await firebase.database().ref(`users/${uid}`).orderByKey().once('value');
 };
 
-export const loginUser = async (username, password) => {
+export const loginUser = async (username: string, password: string) => {
   const data = await firebase.auth().signInWithEmailAndPassword(username, password);
   const userInfo = await getUserInfo(data.uid);
   return {...data.toJSON(), colorId: userInfo.val().colorId, chats: userInfo.val().chats };
 };
 
-export const createUser = async (username, password) => {
+export const createUser = async (username: string, password: string) => {
   const data = await firebase.auth().createUserWithEmailAndPassword(username, password);
   const userInfo = await getUserInfo(data.uid);
   let userInfoData = userInfo.val();
@@ -22,7 +24,7 @@ export const createUser = async (username, password) => {
   return {...data.toJSON(), colorId: userInfoData.colorId};
 };
 
-export const searchByColorId = async (colorId) => {
+export const searchByColorId = async (colorId: string) => {
   return await firebase.database().ref('users').orderByChild('colorId').equalTo(colorId).once('value');
 };
 
