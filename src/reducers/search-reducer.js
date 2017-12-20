8e0f4049dfa8b841e83b26d238ca034ec8d15820
Ozan Kasikci <kasikciozan@yandex.com>
2017-12-21 01:15:41 +0300

    search-reducer: add type definitions

--- src/reducers/search-reducer.js
+++ src/reducers/search-reducer.js
@@ -1,4 +1,15 @@
-import { types } from '../actions/index'
+// @flow
+
+import { get } from 'lodash';
+import { types } from '../actions/index';
+import type { actionType } from '../flow-types';
+
+type initialStateType = {
+  colorId: string,
+  selectedColor: string,
+  currentChar: string,
+  foundUser: ?Object,
+}
 
 const initialState = {
   colorId: '',
@@ -7,18 +18,19 @@ const initialState = {
   foundUser: null,
 };
 
-export default function(state = initialState, action) {
+export default function(state: initialStateType = initialState, action: actionType) {
+  const payload = get(action, 'payload', {});
 
   switch (action.type) {
 
     case types.SEARCH_SELECT_COLOR:
-      return { ...state, selectedColor: action.payload.color };
+      return { ...state, selectedColor: payload.color };
 
     case types.SEARCH_CHANGE_COLOR_ID:
-      return { ...state, colorId: action.payload.colorId };
+      return { ...state, colorId: payload.colorId };
 
     case types.SEARCH_FOUND_BY_COLOR_ID:
-      return { ...state, foundUser: action.payload.data };
+      return { ...state, foundUser: payload.data };
 
     default:
       return state
