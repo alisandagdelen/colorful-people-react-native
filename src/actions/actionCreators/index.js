720335f0e21da82d08ef480ecca1bb2898820c2b
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-01-14 00:02:11 +0300

    - New action creators

--- src/actions/actionCreators/index.js
+++ src/actions/actionCreators/index.js
@@ -1,7 +1,19 @@
-import {PROFILE_SELECT} from "../types" ;
+import {PROFILE_SELECT, BEGIN_EDITING, END_EDITING} from "../types" ;
 
 const profileSelected = () => {
     return {
         type: PROFILE_SELECT,  
     }
-}
+};
+
+const beginEditing = () => {
+    return {
+        type: BEGIN_EDITING,  
+    }
+};
+
+const endEditing = () => {
+    return {
+        type: END_EDITING,  
+    }
+};
