97deb029c3a44eba18b342da3f01957498f9d0c4
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-01-14 01:15:05 +0300

    - UI update

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,44 +1,46 @@
-// import { Header, Container, Title, Content, List, ListItem, InputGroup, Input, Icon, Text, Picker, Button } from 'native-base';
-// import React from 'react';
+import { Header, Container, Title, Content, List, ListItem, InputGroup, Input, Icon, Text, Picker, Button } from 'native-base';
+import React from 'react';
+import styles from './styles'
 
-// class Profile extends React.Component {
+class Profile extends React.Component {
 
-//   render() {
-//     return (
-//       <Container style={{backgroundColor: 'white'}}>
+    render() {
+        return (
+            <Container style={{ backgroundColor: 'white' }}>
 
-//         <Content>
-//           <List>
-//             <ListItem>
-//               <InputGroup>
-//                 <Icon name="ios-person" style={{ color: '#0A69FE' }} />
-//                 <Input
-//                   value={this.props.username}
-//                   onChangeText={text => this.props.changeUsername(text)}
-//                   placeholder={"Email Address"} />
-//               </InputGroup>
-//             </ListItem>
-//             <ListItem>
-//               <InputGroup>
-//                 <Icon name="ios-unlock" style={{ color: '#0A69FE' }} />
-//                 <Input
-//                   value={this.props.password}
-//                   onChangeText={text => this.props.changePassword(text)}
-//                   secureTextEntry={true}
-//                   placeholder={"Password"} />
-//               </InputGroup>
-//             </ListItem>
-//           </List>
-//           <Button onPress={() => this.props.signIn(this.props.username, this.props.password)}
-//                   style={styles.primaryButton} >
-//             <Text>Login</Text>
-//           </Button>
-//           <Button onPress={() => this.props.navigation.navigate('Signup')} style={styles.primaryButton}>
-//             <Text>New to here?</Text>
-//           </Button>
-//         </Content>
-//       </Container>)
-//   }
-// }
+                <Content>
+                    <List>
 
-// export default Profile
+                        <View style={styles.logo}>
+                            <Thumbnail large source={{ uri: 'https://images.unsplash.com/reserve/Af0sF2OS5S5gatqrKzVP_Silhoutte.jpg' }} />
+                        </View>
+
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-person" style={{ color: '#0A69FE' }} />
+                                <Input
+                                    value={this.props.username}
+                                    onChangeText={text => this.props.nickname(text)}
+                                    placeholder={"Email Address"} />
+                            </InputGroup>
+                        </ListItem>
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-unlock" style={{ color: '#0A69FE' }} />
+                                <Input
+                                    value={this.props.bio}
+                                    onChangeText={text => this.props.bio(text)}
+                                    placeholder={"bio"} />
+                            </InputGroup>
+                        </ListItem>
+                    </List>
+                    <Button onPress={() => this.props.updated()}
+                        style={styles.primaryButton} >
+                        <Text>Login</Text>
+                    </Button>
+                </Content>
+            </Container>)
+    }
+}
+
+export default Profile
---
+++ src/components/profile/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export default  StyleSheet.create({
+    logo: {
+        alignSelf:'center',
+        margin: 30,
+    },
+    primaryButton: {
+      margin: 13,
+      marginTop: 30,
+    },
+    facebookButton : {
+        marginTop:120,
+        padding: 10,
+        alignSelf: 'center',
+        backgroundColor: '#3B5998',
+    },
+    footerText : {
+        alignSelf: 'center',
+        color: '#FF6265',
+        marginTop: 40
+    },
+    signUp : {
+        alignSelf: 'center',
+    }
+  });
